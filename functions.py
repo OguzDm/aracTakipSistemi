a26d542a8844bcaf8e4df43f0d6211bd06224a14
Oğuz Demirhan <33404285+OguzDm@users.noreply.github.com>
2019-11-30 14:39:59 +0300

    fonksiyonlar için dosya eklendi

---
+++ functions.py
@@ -0,0 +1,27 @@
+from Car import Car_T
+
+erisimListesi = [Car_T() for i in range(1000)]
+
+def ekleme(sayi):                                               #Erişim listesine kayıt eklemek için oluşturulmuş bir fonksiyon
+
+
+    erisimListesi[sayi].arac_ureticisi = input("Lütfen Aracınızın markasını giriniz")
+    erisimListesi[sayi].arac_modeli = input("Lütfen Aracınızın modelini giriniz")
+    erisimListesi[sayi].renk = input("Lütfen Aracınızın rengini giriniz")
+    erisimListesi[sayi].motor_no = int(input("Lütfen Aracınızın motor no'sunu giriniz"))
+    erisimListesi[sayi].plaka = input("Lütfen Aracınızın plakasını giriniz")
+    erisimListesi[sayi].sasi_no = int(input("Lütfen aracınızın sasi numarasını giriniz"))
+
+def dosyaYaz(sayi):
+    f = open("testfile.txt","a")            #Dosya ekleme modunda açıldı.
+    f.write(erisimListesi[sayi].arac_ureticisi + " ")
+    f.write(erisimListesi[sayi].arac_modeli + " ")
+    f.write(erisimListesi[sayi].renk + " ")
+    f.write(str(erisimListesi[sayi].motor_no))
+    f.write(" " + erisimListesi[sayi].plaka + " ")
+    f.write(str(erisimListesi[sayi].sasi_no))
+    f.write("\n")
+    f.close()
+
+
+
